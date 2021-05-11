235e657b0de0f01a771e40b0d61b1fd60b2d0946
Marie Amurao <mamurao@g.clemson.edu>
2021-05-11 03:56:37 -0400

    figuring out gh pages

---
+++ Mid-Mod2_ReactApp_Food/food_app/src/config.js
@@ -0,0 +1,13 @@
+// API keys
+// file is listed in .gitignore so keys won't be uploaded
+// and publically visible
+
+// confirm that config.js is not going to be included in [git add] by doing [git status] first and then making sure config.js is not in 
+// Untracked files that'll be included in what will be commited
+
+var config = {
+    edamam: {key:"9281d45f94cf8726dec208f17c54fcb1", ID:"e6f0a3f9"},
+    newsAPI: '830f5aaa56374b91b27c3e5a138af549',
+}
+
+export default config;
